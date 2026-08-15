63562cd25af9a3582be64b26f6a1bee980309ac8
GauravVerma789 <gauruv54@gmail.com>
2026-08-15 12:52:43 +0530

    controllers and routes

--- src/app.js
+++ src/app.js
@@ -24,7 +24,9 @@ app.use(express.static("public"))
 app.use(cookieParser())
 // bas cookieparser use karne k liye 
 
- 
+// routes dalenge ab
 
+import userRouter from "./routes/user.routes.js"
 
+app.use("/api/v1/users",userRouter)
 export { app }
